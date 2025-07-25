7bae0d9f363c357117b2092c068d33671af15a78
irony-cte <cartertelias@outlook.com>
2025-07-25 15:44:14 -0500

    add --ignore-repo

--- functions/cli_tools.sh
+++ functions/cli_tools.sh
@@ -35,5 +35,26 @@ case "$1" in
         fi
         exit 0
         ;;
+    --ignore-repo)
+        #  Check if the ignore pattern is provided
+        if [ -z "$2" ]; then
+            echo "Error: No ignore pattern provided."
+            echo "Usage: gitit --ignore-repo <REGEX pattern>"
+            exit 1
+        fi
+        #  Check if the ignore.conf file exists, if not, create it
+        ignore_pattern="$2"
+        if [ ! -f "$HOME/.config/gitit/ignore.conf" ]; then
+            mkdir -p "$HOME/.config/gitit"
+            touch "$HOME/.config/gitit/ignore.conf"
+        fi
+        #  Append the ignore pattern to the ignore.conf file
+        if [ -f "$repolist" ]; then
+            grep -E "$ignore_pattern" "$repolist" >> "$HOME/.config/gitit/ignore.conf"
+            echo ignore pattern "$ignore_pattern" added to "$HOME"/.config/gitit/ignore.conf
+        else
+            echo "No repositories found."
+        fi
+        ;;
 esac
 
