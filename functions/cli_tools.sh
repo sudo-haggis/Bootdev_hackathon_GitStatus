082656dbf77fa04e2320c3969442b7daac4fbfb1
irony-cte <cartertelias@outlook.com>
2025-07-25 14:49:43 -0500

    added logic for removing exisiting temp list

--- functions/cli_tools.sh
+++ functions/cli_tools.sh
@@ -2,6 +2,10 @@
 
 #  search the computer for repositories, adding them to a .txt file
 echo "Getting repositories..."
+##  checks if the temporary repositories.txt file exists, if so, removes it
+if [ -f "/tmp/repositories.txt" ]; then
+    rm "/tmp/repositories.txt"
+fi
 ##  sets the search directory to the home directory
 SEARCH_DIR="$HOME"
 ##  searches for .git directories
@@ -11,3 +15,5 @@ find "$SEARCH_DIR" -type d -name ".git" 2>/dev/null | while read -r git_dir; do
     ##  appends the repository path to a temporary repositories.txt file
     echo "$repo_dir" >> "/tmp/repositories.txt"
 done
+
+repolist="/tmp/repositories.txt"
