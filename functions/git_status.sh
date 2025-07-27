d0f9abec5c23159061d96bac274f2678be00d841
irony-cte <cartertelias@outlook.com>
2025-07-27 18:09:07 -0500

    feature(git_status): added status function
    Just went and added exactly what we needed, not sure if this is the proper work flow but it's crunch time

---
+++ functions/git_status.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+git_status() {
+    #param1 is directory path to deal with 
+    #move to the directory specified
+    abs_path=$1
+    cd $abs_path
+
+    #if we find a .git file, 
+    if ! git rev-parse --is-inside-work-tree >/dev/null 2>&1; then
+        echo "Not a repo" && return 1
+    else 
+        #show some details
+        local repo_name=$(basename $(git rev-parse --show-toplevel))
+        local repo_curr_branch=$(git branch --show-current)
+        echo "Found $repo_name on branch $repo_curr_branch"
+
+        local staged=$(git diff --cached --name-only | wc -l)
+        local modified=$(git diff --name-only | wc -l)
+
+        echo "Files: ${staged} staged, ${modified} modified"
+        echo "" #new line for formatting
+
+        if [ $staged -gt 0 ] || [ $modified -gt 0 ]; then
+            git status -s --untracked-files=no | head -8 
+        else
+            echo "Clean tree"
+        fi
+
+        return 0
+    fi
+}
