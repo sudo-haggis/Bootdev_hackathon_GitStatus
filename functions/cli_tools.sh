6920739f2b52619a3c77f5f832bab44f11d51df0
irony-cte <cartertelias@outlook.com>
2025-07-26 23:51:15 -0500

    refactor(cli_tools.sh) implemented git_status.sh & added new flags giving direct access to certain functions

--- functions/cli_tools.sh
+++ functions/cli_tools.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 #  gitIt CLI Tool
 #  This script provides a command-line interface to manage and print reports of local Git repositories.
+source ./git_status.sh
 
 __init__() {
     ##  Establishes variables for the script  ##
@@ -13,6 +14,8 @@ __init__() {
     repo_list="/tmp/repositories.txt"
     ##  sets the path for the report file
     report="/tmp/repositories_report.txt"
+    ##  sets the path for the git status output file
+    status="/tmp/git_status_output.txt"
 
 ## removes the temporary repositories.txt file if it exists
     if [ -f "$repo_list" ]; then
@@ -23,32 +26,34 @@ __init__() {
     if [ -f "$report" ]; then
     rm "$report"
     fi
-}
+    
+##  generates sorted list of repositories
 
-quick_report() {
-##  Generates a quick report of all local repositories found in the search directory
-    echo "==============================================================================="
-    echo
-    echo "*gitting* repositories..."
-    echo
-    echo "==============================================================================="
+    echo "                              *gitting* repositories..."
+    echo "---------------------------------------------------------------------------------------"
     ##  searches for .git directories
     find "$SEARCH_DIR" -type d -name ".git" 2>/dev/null | while read -r git_dir; do
     ##  gets the parent directory of the .git directory
     repo_dir=$(dirname "$git_dir")
     ## checks the ignore.conf file for the repository path, ignoring it if present
     if grep -q "$repo_dir" "$ignore_list" 2>/dev/null; then
-        echo "Ignoring repository: $repo_dir..."
+        echo "INFO: Ignoring $repo_dir!"
         continue
     else
     ##  appends the repository path to a temporary repositories.txt file
         echo "$repo_dir" >> "$repo_list"
     fi
     done
+    echo "------------------------------------------------------------------------------------"
     ## sorts the repository list and removes duplicates
     sort -u "$repo_list" -o "$repo_list"
     ## creates report text file
     nl -w 2 -s '. ' "$repo_list" > "$report"
+}
+
+quick_report() {
+##  Generates a quick report of all local repositories found in the search directory
+
 
 ##  prints a quick report of the repositories found
     echo "==============================================================================="
@@ -79,7 +84,7 @@ move_to_repo() {
 
     if [ -n "$target_repo" ]; then
             echo "Changing directory to: $target_repo"
-            cd "$target_repo" || echo "Failed to change directory."
+            eval cd "$target_repo" || echo "Failed to change directory."
         fi
 
 }
@@ -116,13 +121,28 @@ case "$1" in
         exit 0
         ;;
     -r|--report)
-        if [ -f "$repo_list" ]; then
-            echo "Generating report of all repositories..."
-            sleep 1
-            less "$repo_list"
-        else
-            echo "No repositories found."
-        fi
+        {
+            if [ -f "$repo_list" ]; then
+                echo "Generating report of all repositories..."
+                echo "==============================================================================="
+                echo
+                echo "Found $(wc -l "$repo_list" | awk '{print $1}') active repositories"
+                echo "-----------------------"
+                echo "List of repositories:"
+                echo "-----------------------"
+                cat "$report"                 
+                echo
+                echo "                              git status"
+                echo "==============================================================================="
+                for repo in $(cat "$repo_list"); do
+                    git_status "$repo" > "$status"
+                done
+                cat "$status"
+                echo "==============================================================================="
+            else
+                echo "No repositories found."
+            fi
+        } | less
         exit 0
         ;;
     --ignore-repo)
@@ -146,6 +166,25 @@ case "$1" in
         else
             echo "No repositories found."
         fi
+        ;;
+    -m|--move-to)
+        cat "$report"
+        move_to_repo
+        exit 0
+        ;;
+    -s|--status)
+        if [ -f "$repo_list" ]; then
+            echo "             Generating git status for all repositories..."
+            echo "==============================================================================="
+            for repo in $(cat "$repo_list"); do
+                git_status "$repo" > "$status"
+            done
+            cat "$status"
+            echo "==============================================================================="
+        else
+            echo "No repositories found."
+        fi
+        exit 0
         ;;
     -t|--test-dir)
         SEARCH_DIR="."
