93f6dd447ac7c67c1536c113ecf009ec6dfa365a
sudo-haggis <156850571+sudo-haggis@users.noreply.github.com>
2025-07-27 14:07:20 +0200

    chore(cleanup): removed pointless files we wont use

--- functions/search.py
+++
@@ -1,7 +0,0 @@
-import os
-with open("git_repos.txt", "w") as f:
-        for root, dirs, files in os.walk("."):
-            if ".git" in dirs:
-                f.write(os.path.abspath(root) + "\n")
-
-print("Git repo paths saved")
