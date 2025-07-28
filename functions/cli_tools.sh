1819ee179c43bc855b09ea882b34ef326ea0f7d4
sudo-haggis <156850571+sudo-haggis@users.noreply.github.com>
2025-07-28 18:10:54 +0200

    feat(cli_tools): new branch, new fresh tools looks - included colours - slimed down on new lines - generate_status function added - more ...

--- functions/cli_tools.sh
+++ functions/cli_tools.sh
@@ -2,11 +2,17 @@
 #  gitIt CLI Tool
 #  This script provides a command-line interface to manage and print reports of local Git repositories.
 source ./functions/git_status.sh
+
+
 __init__() {
     ##  Establishes variables for the script  ##
 
     ##  sets the search directory to the home directory
-    SEARCH_DIR="$HOME"
+    # or runs only in testing fake_ directory
+    if [ ! $TEST ]; then
+        SEARCH_DIR="$HOME"
+    fi
+
     ##  sets the ignore list file path
     ignore_list="$HOME/.config/gitit/ignore.conf"
     ##  sets the path for the temporary repository list file
@@ -18,31 +24,30 @@ __init__() {
 
 ## removes the temporary repositories.txt file if it exists
     if [ -f "$repo_list" ]; then
-    rm "$repo_list"
+        rm "$repo_list"
     fi
 
 ## removes the report file if it exists
     if [ -f "$report" ]; then
-    rm "$report"
+        rm "$report"
     fi
     
 ##  generates sorted list of repositories          
     echo "                              *gitting* repositories..."
-    echo "-----------------------------------------------------------------------------------------"
     ##  searches for .git directories
     find "$SEARCH_DIR" -type d -name ".git" 2>/dev/null | while read -r git_dir; do
-    ##  gets the parent directory of the .git directory
-    repo_dir=$(dirname "$git_dir")
-    ## checks the ignore.conf file for the repository path, ignoring it if present
-    if grep -q "$repo_dir" "$ignore_list" 2>/dev/null; then
-        echo "INFO: Ignoring $repo_dir!"
-        continue
-    else
-    ##  appends the repository path to a temporary repositories.txt file
-        echo "$repo_dir" >> "$repo_list"
-    fi
+        ##  gets the parent directory of the .git directory
+        repo_dir=$(dirname "$git_dir")
+        ## checks the ignore.conf file for the repository path, ignoring it if present
+        if grep -q "$repo_dir" "$ignore_list" 2>/dev/null; then
+            echo "INFO: Ignoring $repo_dir!"
+            continue
+        else
+        ##  appends the repository path to a temporary repositories.txt file
+            echo "$repo_dir" >> "$repo_list"
+        fi
     done
-    echo "-----------------------------------------------------------------------------------------"
+
     ## sorts the repository list and removes duplicates
     sort -u "$repo_list" -o "$repo_list"
     ## creates report text file
@@ -50,20 +55,36 @@ __init__() {
 }
 
 quick_report() {
-##  Generates a quick report of all local repositories found in the search directory
-
-
+##  Generates a quick report of all local repositories found in the search director
 ##  prints a quick report of the repositories found
-    echo "========================================================================================="
-    echo
-    echo "Found $(wc -l "$repo_list" | awk '{print $1}') active repositories"
-    echo
-    echo "========================================================================================="
-    echo
-    echo "List of repositories:"
-    echo "----------------------------------------------------------------------------------------"
-    cat "$report"     
-    echo "========================================================================================="
+    echo -e "${RED}=========================================================================================${NC}"
+    echo "Found $(wc -l "$repo_list" | awk '{print $1}') active repositories:"
+    cat "$report"    
+    echo -e "${RED}=========================================================================================${NC}"
 }
 
+generate_status(){
+    if [ -f "$repo_list" ]; then
+        echo "             Generating git status for all repositories..."
+        echo -e "${RED}=========================================================================================${NC}"
+            echo -e "${GREEN}≡≡≡≡≡≡≡≡≡≡≡≡≡≡${NC}" 
 
+            for repo in $(cat "$repo_list"); do
+                git_status "$repo"
+                echo -e "${GREEN}≡≡≡≡≡≡≡≡≡≡≡≡≡≡${NC}"
+            done
+        echo -e "${RED}=========================================================================================${NC}"
+    else
+        echo "No repositories found."
+    fi
+    exit 0
+}
+
+init_colours() {
+    # Colors for output
+    RED='\033[0;31m'
+    GREEN='\033[0;32m'
+    YELLOW='\033[0;33m'
+    BLUE='\033[0;34m'
+    NC='\033[0m' # No Colors
+}
